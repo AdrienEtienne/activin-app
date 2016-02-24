6fe800657102ae6b5c90b018cb17335418165593
aee <a.etienne@galitt.com>
2016-02-24 16:19:51 +0100

    ajout dependances bower avec gulp

---
+++ www/js/controllers.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Controller: MainController', function () {
+
+	// load the controller's module
+	beforeEach(module('starter.controllers'));
+
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+
+	}));
+
+	it('should attach a list of sports to the controller', function () {
+		expect(2).toBe(2);
+	});
+});
