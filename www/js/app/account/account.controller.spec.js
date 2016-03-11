f3c0949ee14e8cfd9d3dafe4879c89a83939427d
aee <a.etienne@galitt.com>
2016-03-11 12:32:28 +0100

    Add login tests and default values

--- www/js/app/account/account.controller.spec.js
+++ www/js/app/account/account.controller.spec.js
@@ -7,12 +7,6 @@ describe('Controller: AccountCtrl', function () {
 	// load the controller's module
 	beforeEach(module('account.controller'));
 
-	beforeEach(function () {
-		window.localStorage['email'] = 'mail';
-		window.localStorage['password'] = 'password';
-		window.localStorage['token'] = 'token';
-	});
-
 	// Initialize the controller and a mock $window
 	beforeEach(inject(function ($controller, $rootScope, _Auth_, $state) {
 		scope = $rootScope.$new();
@@ -23,22 +17,12 @@ describe('Controller: AccountCtrl', function () {
 		});
 	}));
 
-	it('should have login infos', function () {
-		assert.equal(Auth.hasLogin(), true);
-	});
-
-	it('should logout the user', function () {
-		state.go = function () {};
-		scope.logout();
-		assert.equal(Auth.hasLogin(), false);
-	});
-
 	it('should go to auth', function () {
 		var tmp;
 		state.go = function (name) {
 			tmp = name;
 		}
 		scope.logout();
-		assert.equal(tmp, 'auth');
+		assert.equal(tmp, 'login');
 	});
 });
---
+++ www/js/components/auth/auth.service.spec.js
@@ -0,0 +1,68 @@
+'use strict';
+
+describe('Service: Auth', function () {
+
+	var Auth, $httpBackend;
+
+	// load the controller's module
+	beforeEach(module('components.auth'));
+
+	// Initialize the controller and a mock $window
+	beforeEach(inject(function (_Auth_, _$httpBackend_) {
+		Auth = _Auth_;
+		$httpBackend = _$httpBackend_;
+		window.localStorage['token'] = undefined;
+	}));
+
+	describe('Logout()', function () {
+		it('should not have token', function () {
+			should.not.exist(Auth.getToken());
+		});
+
+		it('should not have password', function () {
+			window.localStorage['token'] = 'token';
+			Auth.logout();
+			should.not.exist(Auth.getToken());
+		});
+	});
+
+	describe('Login/Password/Token', function () {
+		beforeEach(function () {
+			$httpBackend.when('POST', 'http://localhost:9000/auth/local').respond({
+				token: 'mon token'
+			});
+			$httpBackend.when('GET', 'http://localhost:9000/api/users/me').respond({});
+			Auth.login('email', 'password');
+			$httpBackend.flush();
+		});
+
+		it('should have login', function () {
+			Auth.getLogin().should.equal('email');
+		});
+
+		it('should have password', function () {
+			Auth.getPassword().should.equal('password');
+		});
+
+		it('should have token', function () {
+			Auth.getToken().should.equal('mon token');
+		});
+
+		it('should not have login', function () {
+			Auth.logout();
+			should.not.exist(Auth.getLogin());
+		});
+
+		it('should not have password', function () {
+			Auth.logout();
+			should.not.exist(Auth.getPassword());
+		});
+
+		it('should not have token', function () {
+			Auth.logout();
+			should.not.exist(Auth.getToken());
+		});
+
+
+	});
+});
